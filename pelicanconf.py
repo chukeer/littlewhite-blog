1252ef6cc6b96b92e8f04263c08b41e365997f1e
zhangmenghan <zhangmenghan@zhangmenghandeMacBook-Pro.local>
2016-09-06 22:34:20 +0800

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'littlewhite'
+SITENAME = u'LittleWhite'
+SITEURL = 'http://localhost:8000'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DATE_FORMATS = {'zh': '%Y-%m-%d'}
+DEFAULT_LANG = u'zh'
+
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['render_math', 'sitemap', 'extract_toc', 'tipue_search']
+THEME = 'third_themes/pelican-elegant'
+
+# Elegant theme
+MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid', 'toc(permalink=true)']
+DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives', 'search', '404'))
+STATIC_PATHS = ['images', 'theme']
+TAG_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+         'articles': 0.5,
+         'indexes': 0.5,
+         'pages': 0.5
+     },
+     'changefreqs': {
+         'articles': 'monthly',
+         'indexes': 'daily',
+         'pages': 'monthly'
+     }
+}
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+LINK = ()
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
